b9ba59f2d2de806c1585ae18d910c4d892d686e0
pieceofcheese <linhai.yin@mil.utoronto.ca>
2017-03-06 15:50:14 +0000

    Added instructions and more commands

--- bot/poibot.go
+++ bot/poibot.go
@@ -13,13 +13,65 @@ import (
 var (
 	Token string
 	BotId string
-	PoiImage string = "/home/linhai/go/assets/poi.jpg"
+
+	POIINSTRUCTIONS = "poi_instructions: Instructions Poi!\npoi: POI!\nlolicon: For terrible people Poi!\ngo sleep: I'll tell people to sleep!\nfuck: POI!\nmistakes were made: mistakes poi\npoi_lewd: For lewd people poi.\npoi_tired: I'm tired poi.\npoi_memes: Memes poi."
+
+
+	assetRoot string = "/home/linhai/go/assets/"
+	imagePaths [17]string
+
+	lewd [4]string
+	lewdct int = 0
+
+	tired [2]string
+	tiredct int = 0
+
+	meme [3]string
+	memect int = 0
+
+	lolicon [2]string
+	loliconct int = 0
+
 )
 
 func init() {
 
 	flag.StringVar(&Token, "t", "", "Bot Token")
 	flag.Parse()
+
+	// setup globals
+	imagePaths[0] = "poi.jpg"
+        imagePaths[1] = "exodias_d.jpg"
+        imagePaths[2] = "fuck.gif"
+        imagePaths[3] = "go_sleep.png"
+        imagePaths[4] = "i_came.jpg"
+        imagePaths[5] = "last_night.jpg"
+        imagePaths[6] = "lewd_meme.png"
+        imagePaths[7] = "lolicon_rights.png"
+        imagePaths[8] = "meme_oniichan.jpg"
+        imagePaths[9] = "mistakes_were_made.jpg"
+        imagePaths[10] = "monopoly.jpg"
+        imagePaths[11] = "no_peeking.jpg"
+        imagePaths[12] = "oniichan.jpg"
+        imagePaths[13] = "ride.jpg"
+        imagePaths[14] = "tired.jpg"
+        imagePaths[15] = "traps_gay.png"
+        imagePaths[16] = "watching.jpg"
+
+	lewd[0] = imagePaths[1]
+        lewd[1] = imagePaths[4]
+        lewd[2] = imagePaths[13]
+        lewd[3] = imagePaths[16]
+
+	tired[0] = imagePaths[5]
+        tired[1] = imagePaths[14]
+
+	meme[0] = "communism.jpg"
+        meme[1] = imagePaths[6]
+        meme[2] = imagePaths[8]
+
+	lolicon[0] = imagePaths[7]
+        lolicon[1] = imagePaths[10]
 
 }
 
@@ -68,8 +120,17 @@ func main() {
 //Handles any messageCreated events
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
-	poiRegex, _ := regexp.Compile(".*[pP][oO][iI].*")
+	instructionRegex, _ := regexp.Compile("^poi_instructions$")
+	poiRegex, _ := regexp.Compile("^[pP][oO][iI]+$")
 	whatIsPoiRegex, _ := regexp.Compile("[wW][hH][aA][tT] [iI][sS] [pP][oO][iI]\\??")
+	lewdRegex, _ := regexp.Compile("^poi_lewd$")
+	fuckRegex, _ := regexp.Compile("^[fF][uU][cC][kK]$")
+	tiredRegex, _ := regexp.Compile("^poi_tired$")
+	sleepRegex, _ := regexp.Compile("^[gG][oO] [sS][lL][eE]{2}[pP][!]?$")
+	memeRegex, _ := regexp.Compile("^poi_memes$")
+	mistakesRegex, _ := regexp.Compile("^mistakes were made$")
+	loliconRegex, _ := regexp.Compile("^lolicon$")
+	
 
 	// Ignore all messages created by bot
 	// AVOIDS INFINITE CALLS
@@ -80,6 +141,7 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Received a message
         fmt.Printf("%20s %20s %20s > %s\n", m.ChannelID, time.Now().Format(time.Stamp), m.Author.Username, m.Content)
 
+	// User wants to know about poi
 	if whatIsPoiRegex.MatchString(m.Content) {
                 _, err := s.ChannelMessageSend(m.ChannelID, "Poi is poi.")
                 if err != nil {
@@ -88,25 +150,108 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
         }
 
+	// User wants instructions
+	if instructionRegex.MatchString(m.Content) {
+		_, err := s.ChannelMessageSend(m.ChannelID, POIINSTRUCTIONS)
+                if err != nil {
+                        fmt.Println("ERR: failed to send message")
+                }
+                return
+	}
+
+	// POI
 	if poiRegex.MatchString(m.Content) {
-		/*_, err := s.ChannelMessageSend(m.ChannelID, "Poi!")
-		if err != nil {
-			fmt.Println("ERR: Failed to send message,", err)
-		}*/
-
-		image, err := os.Open(PoiImage)
-
-		if err != nil {
-			s.ChannelMessageSend(m.ChannelID, "I couldn't find the image poi :(")
-			fmt.Println("%s\n", PoiImage)
-			return
-		}
-
-		_, err = s.ChannelFileSendWithMessage(m.ChannelID, "", "poi0.jpg", image)
-		if err != nil {
-			fmt.Println("ERR: Failed to send message,", err)
-			return
-		}
+		singleMessage(s, m, imagePaths[0])
 		return
 	}
+
+	// lewds
+	if lewdRegex.MatchString(m.Content) {
+		manyMessage(s, m, lewd[:], &lewdct)
+		return
+	}
+
+	// fuck
+	if fuckRegex.MatchString(m.Content) {
+		singleMessage(s, m, imagePaths[2]) 
+                return
+	}
+
+	// tired
+	if tiredRegex.MatchString(m.Content) {
+		manyMessage(s, m, tired[:], &tiredct)
+                return
+        }
+
+	// go sleep
+	if sleepRegex.MatchString(m.Content) {
+		singleMessage(s, m, imagePaths[3])
+                return
+	}
+
+	// meme
+        if memeRegex.MatchString(m.Content) {
+		manyMessage(s, m, meme[:], &memect)
+                return
+        }
+
+	// lolicon
+        if loliconRegex.MatchString(m.Content) {
+		manyMessage(s, m, lolicon[:], &loliconct)
+                return
+        }
+
+	// mistakes
+	if mistakesRegex.MatchString(m.Content) {
+		singleMessage(s, m, imagePaths[9])
+                return
+        }
+
+}
+
+func manyMessage(s *discordgo.Session, m *discordgo.MessageCreate, imgs []string, ct *int) bool {
+
+    if(*ct+1 >= len(imgs)) {
+        *ct = 0
+    } else {
+       *ct += 1
+    }
+
+    image, err := os.Open(assetRoot + imgs[*ct])
+
+    if err != nil {
+        s.ChannelMessageSend(m.ChannelID, "I couldn't find the image poi :(")
+        fmt.Println("%s\n", assetRoot + imgs[*ct])
+        return false
+    }
+
+    _, err = s.ChannelFileSendWithMessage(m.ChannelID, "", imgs[*ct], image)
+    if err != nil {
+        fmt.Println("ERR: Failed to send message,", err)
+        return false
+    }
+
+    return true
+
+
+}
+
+func singleMessage(s *discordgo.Session, m *discordgo.MessageCreate, img string) bool {
+
+    image, err := os.Open(assetRoot + img)
+
+    if err != nil {
+        s.ChannelMessageSend(m.ChannelID, "I couldn't find the image poi :(")
+        fmt.Println("%s\n", assetRoot + img)
+        return false
+    }
+
+    _, err = s.ChannelFileSendWithMessage(m.ChannelID, "", img, image)
+    if err != nil {
+        fmt.Println("ERR: Failed to send message,", err)
+        return false
+    }
+
+    return true
+
 }
